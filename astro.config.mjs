d42aaa3793f87b229d8249f53a3bd21fc712fc31
Seth <sethetherald@duck.com>
2024-12-01 22:17:47 +0700

    update: use img tag for cover url instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
-import sitemap from "@astrojs/sitemap";
-import { imageService } from '@unpic/astro/service';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://etherald.dev",
-  integrations: [mdx(), sitemap()],
-  image: {
-    service: imageService(),
-  },
+	site: 'https://etherald.dev',
+	integrations: [mdx(), sitemap()],
 });
